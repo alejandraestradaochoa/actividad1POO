7f73b9e9a50bb3955257a5ce14a0bf368718afb2
alejandraestradaochoa <159496005+alejandraestradaochoa@users.noreply.github.com>
2024-02-18 23:57:32 -0500

    Update ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+def main():
+    nombre = input("Ingresa tu nombre: ")
+
+    edad = input("Ingresa tu edad: ")
+
+    print("Tu nombre es:", nombre)
+    print("Tu edad es:", edad)
+
+if __name__ == "__main__":
+    main()
